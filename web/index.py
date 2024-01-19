f4d0afb269a6ae9690a43414fddd7fc4c11b423c
dzlabsch <34076978+dzlabsch@users.noreply.github.com>
2024-01-19 17:34:56 +0100

    Update index.py

--- web/index.py
+++ web/index.py
@@ -28,9 +28,9 @@ with tracer().start_as_current_span("home_page", attributes=baggage_as_attribute
             Page("web/index.py", "Home", "🏠"),
             Page("web/signup.py", "signup"),
             Page("web/verify.py", "verify"),
-            Page("web/personal_chat.py", "General_Chat"),
-            Page("web/shared_ask.py", "Ask_Shared_Documents"),
-            Page("web/shared_spaces.py", "List_Shared_Spaces"),
+            Page("web/personal_chat.py", "Chat"),
+            Page("web/shared_ask.py", "Documents_Chat"),
+            Page("web/shared_spaces.py", "List_Shared_Datarooms"),
             Page("web/embed.py", "widget"),
             Page("web/admin/index.py", "Admin_Section", icon="💂"),
             Section("ML Engineering", icon="💻"),
@@ -42,22 +42,22 @@ with tracer().start_as_current_span("home_page", attributes=baggage_as_attribute
 
     login_container = st.container()
 
-    st.subheader("Welcome to Docq - Private & Secure AI Knowledge Insight, your second brain.")
+    st.subheader("Welcome to SecureGPT - Private & Secure AI Datarooms and Chat.")
 
     st.markdown(
         """
-    - Click on _General Chat_ to use Docq like ChatGPT.
-    - Click on _Ask Shared Documents_ link to ask questions and get answers from documents shared within your organisation as a Space.
-    - Click on _Admin Spaces_ to create a new Space, add documents, and share with your organisation.
+    - Click on _General Chat_ to use SecureGPT like ChatGPT.
+    - Click on _Documents_Chat_ link to ask questions and get answers from documents shared within your organisation as a Dataroom.
+    - Click on _Admin Spaces_ to create a new Dataroom, add documents, and share with your organisation.
     """
     )
 
     st.subheader("Tips & Tricks")
     st.markdown(
         """
-    - Always ask questions in plain English and try to be as specific as possible.
-    - Admins can manage the documents in a Space which sets the context for your questions.
-    - Your access to shared spaces is subject to permissions set by your organisation admin.
+    - Always ask questions in your language and try to be as specific as possible.
+    - Admins can manage the documents in a Dataroom which sets the context for your questions.
+    - Your access to shared Datarooms is subject to permissions set by your organisation admin.
     - For any questions or feedback, please contact your organisation's Docq administrator.
 
     Enjoy Docq!
@@ -67,7 +67,7 @@ with tracer().start_as_current_span("home_page", attributes=baggage_as_attribute
 
     st.markdown(
         """
-    [Website](https://docq.ai) | [Docs](https://docqai.github.io/docq/) | [Github](https://github.com/docqai) | [Twitter](https://twitter.com/docqai)
+    [Website](https://securegpt.ch) | [Docs](https://securegpt.ch/product/)
         """
     )
 
